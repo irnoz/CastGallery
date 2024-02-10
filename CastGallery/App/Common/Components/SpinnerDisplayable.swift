c97d0e46c9bccfac0fe5011dc886c62870090f3b
irakli nozadze <iraklinozadze2509@gmail.com>
2024-02-10 23:42:50 +0400

    add spinner

---
+++ CastGallery/App/Common/Components/SpinnerDisplayable.swift
@@ -0,0 +1,50 @@
+//
+//  Spinner.swift
+//  CastGallery
+//
+//  Created by Irakli Nozadze on 10.02.24.
+//
+
+import UIKit
+
+protocol SpinnerDisplayable: AnyObject {
+    func showSpinner()
+    func hideSpinner()
+}
+
+extension SpinnerDisplayable where Self : UIViewController {
+    func showSpinner() {
+        guard doesNotExistAnotherSpinner else { return }
+        configureSpinner()
+    }
+
+    private func configureSpinner() {
+        let containerView = UIView()
+        containerView.tag = ViewValues.tagIdentifierSpinner
+        parentView.addSubview(containerView)
+        containerView.fillSuperView()
+        containerView.backgroundColor = .black.withAlphaComponent(ViewValues.opacityContainerSpinner)
+        addSpinnerIndicatorToContainer(containerView: containerView)
+    }
+
+    private func addSpinnerIndicatorToContainer(containerView: UIView) {
+        let spinner = UIActivityIndicatorView(style: .large)
+        spinner.startAnimating()
+        containerView.addSubview(spinner)
+        spinner.centerXY()
+    }
+
+    func hideSpinner(){
+        if let foundView = parentView.viewWithTag(ViewValues.tagIdentifierSpinner) {
+            foundView.removeFromSuperview()
+        }
+    }
+
+    private var doesNotExistAnotherSpinner: Bool {
+        parentView.viewWithTag(ViewValues.tagIdentifierSpinner) == nil
+    }
+
+    private var parentView: UIView {
+        navigationController?.view ?? view
+    }
+}
